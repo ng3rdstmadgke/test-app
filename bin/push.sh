a98f6486bbee8340c0fb8d59b48763111a75370d
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-07-15 16:19:27 +0900

    update src image

--- bin/push.sh
+++ bin/push.sh
@@ -4,12 +4,14 @@ ROOT_DIR="$(cd $(dirname $0)/..; pwd)"
 cd $ROOT_DIR
 docker build --rm -f docker/Dockerfile -t test-app:latest .
 
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 674582907715.dkr.ecr.ap-northeast-1.amazonaws.com
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
+
+
+docker tag test-app:latest ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:latest
+docker push ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:latest
 
 VERSION=$(date +"%Y%m%d.%H%M")
-
-docker tag test-app:latest 674582907715.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:latest
-docker push 674582907715.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:latest
-
-docker tag test-app:latest 674582907715.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:${VERSION}
-docker push 674582907715.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:${VERSION}
+docker tag test-app:latest ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:${VERSION}
+docker push ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/test-app:${VERSION}
